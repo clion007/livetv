a5ddb44cedbd8279f94a88a20b0a92cd37984bc3
clion007 <clion007@126.com>
2024-07-07 03:08:48 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 curl https://github.com/fanmingming/live/raw/main/tv/m3u/ipv6.m3u -sLSo ./m3u/ipv6
 cat ./m3u/ipv6 | grep "tvg-name" > tmp
-while read line
+while read -r line
 do
   id=$(echo $line | awk '{printf $2}' | awk -F "\"" '{printf $2}')
-  sed -i "s/tvg-name=\""${id}"\"/tvg-name=\""${id}"\" tvg-id=\""${id}"\"/g" ipv6
+  sed -i "s/tvg-name=\""${id}"\"/tvg-name=\""${id}"\" tvg-id=\""${id}"\"/g" ./m3u/ipv6
 done < tmp
 rm -f tmp
 if ( ! cmp -s ./m3u/ipv6 ./m3u/ipv6.m3u ); then
