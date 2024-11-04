9adf0222cfef5679acb952595cb693f4bd86a6f4
clion007 <clion007@126.com>
2024-11-04 23:07:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl https://github.com/fanmingming/live/raw/main/tv/m3u/ipv6.m3u -sLSo ./m3u/ipv6
+curl https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/ipv6.m3u -sLSo ./m3u/ipv6
 cat ./m3u/ipv6 | grep "tvg-name" > tmp
 while read -r line
 do
